aa64b9e1c9de8d2e250f3b3ef9d4b3a1ea75548d
nicoleavans <cnicoleavans@live.com>
2024-06-13 11:33:29 -0600

    split MPI & KokkosComm versions into separate methods

--- perf_tests/test_osu_latency.cpp
+++ perf_tests/test_osu_latency.cpp
@@ -22,41 +22,39 @@
 #include "KokkosComm.hpp"
 
 template <typename Space, typename View>
-void osu_latency(benchmark::State &, MPI_Comm comm, const Space &space, int rank, const View &v){
-    bool usingMPI, usingKokkosComm; //TODO where do I want this to come from
+void osu_latency_Kokkos_Comm(benchmark::State &, MPI_Comm comm, const Space &space, int rank, const View &v){
+    double t_total = 0.0;
+    if(rank == 0){
+        Kokkos::Timer timer; //TODO what to do w/ timer given benchmark utils?
+        KokkosComm::send(space, v, 0, 0, comm);
+        KokkosComm::recv(space, v, 0, 0, comm);
+        t_total = timer.seconds();
+        timer.reset();
+    } else if (rank == 1){
+        KokkosComm::recv(space, v, 0, 0, comm);
+        KokkosComm::send(space, v, 0, 0, comm);
+    }
+    if(rank == 0){
+        double latency = t_total * 1e6; //TODO where is 1e6 coming from?
+    }
+}
+
+template <typename Space, typename View>
+void osu_latency_MPI(benchmark::State &, MPI_Comm comm, const Space &space, int rank, const View &v){
     double t_start = 0.0, t_end = 0.0, t_total = 0.0;
-    if(usingMPI){ //TODO separate into two methods? just run both and compare?
-        MPI_Barrier(MPI_COMM_WORLD);
-        t_total = 0.0;
-        if(rank == 0){
-            t_start = MPI_Wtime();
-            //MPI_Send(s_buf, num_elements, curr_datatype, 1, 1, comm); 
-            //MPI_Recv(r_buf, num_elements, curr_datatype, 1, 1, comm, &reqstat); 
-            t_end = MPI_Wtime();
-            t_total += (t_end - t_start);
-        } else if (rank == 1){
-            //MPI_Recv(r_buf, num_elements, curr_datatype, 0, 1, comm, &reqstat);
-            //MPI_Send(s_buf, num_elements, curr_datatype, 0, 1, comm);
-        }
-        if(rank == 0){
-            double latency = t_total * 1e6; //TODO where is 1e6 coming from?
-        }
-    } else if(usingKokkosComm){ //TODO probably just use else
-        MPI_Barrier(MPI_COMM_WORLD);
-        t_total = 0.0;
-        if(rank == 0){
-            Kokkos::Timer timer; //TODO what to do w/ timer given benchmark utils?
-            KokkosComm::send(space, v, 0, 0, comm);
-            KokkosComm::recv(space, v, 0, 0, comm);
-            t_total = timer.seconds();
-            timer.reset();
-        } else if (rank == 1){
-            KokkosComm::recv(space, v, 0, 0, comm);
-            KokkosComm::send(space, v, 0, 0, comm);
-        }
-        if(rank == 0){
-            double latency = t_total * 1e6; //TODO where is 1e6 coming from?
-        }
+    MPI_Barrier(MPI_COMM_WORLD);
+    if(rank == 0){
+        t_start = MPI_Wtime();
+        //MPI_Send(s_buf, num_elements, curr_datatype, 1, 1, comm); 
+        //MPI_Recv(r_buf, num_elements, curr_datatype, 1, 1, comm, &reqstat); 
+        t_end = MPI_Wtime();
+        t_total += (t_end - t_start);
+    } else if (rank == 1){
+        //MPI_Recv(r_buf, num_elements, curr_datatype, 0, 1, comm, &reqstat);
+        //MPI_Send(s_buf, num_elements, curr_datatype, 0, 1, comm);
+    }
+    if(rank == 0){
+        double latency = t_total * 1e6; //TODO where is 1e6 coming from?
     }
 }
 
@@ -76,7 +74,7 @@ void benchmark_osu_latency(benchmark::State &state){
     view_type a("", 1000000);
 
     while(state.KeepRunning()){
-        do_iteration(state, MPI_COMM_WORLD, osu_latency<Kokkos::DefaultExecutionSpace, view_type>, space, rank, a);
+        do_iteration(state, MPI_COMM_WORLD, osu_latency_Kokkos_Comm<Kokkos::DefaultExecutionSpace, view_type>, space, rank, a);
     }
 
     state.SetBytesProcessed(sizeof(Scalar) * state.iterations() * a.size() * 2);
