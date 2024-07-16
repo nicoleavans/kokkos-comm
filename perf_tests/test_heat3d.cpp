9a20ec2f5a64a94dc09a6f7414fd2ffeae6c0272
nicoleavans <cnicoleavans@live.com>
2024-07-16 15:42:11 -0600

    remove reference to SpaceInstance

--- perf_tests/test_heat3d.cpp
+++ perf_tests/test_heat3d.cpp
@@ -2,36 +2,6 @@
 #include "test_utils.hpp"
 #include <iostream>
 
-template <class ExecSpace>
-struct SpaceInstance {
-  static ExecSpace create() { return ExecSpace(); }
-  static void destroy(ExecSpace&) {}
-  static bool overlap() { return false; }
-};
-
-#ifdef KOKKOS_ENABLE_CUDA
-template <>
-struct SpaceInstance<Kokkos::Cuda> {
-  static Kokkos::Cuda create() {
-    cudaStream_t stream;
-    cudaStreamCreate(&stream);
-    return Kokkos::Cuda(stream);
-  }
-  static void destroy(Kokkos::Cuda& space) {
-    cudaStream_t stream = space.cuda_stream();
-    cudaStreamDestroy(stream);
-  }
-  static bool overlap() {
-    bool value          = true;
-    auto local_rank_str = std::getenv("CUDA_LAUNCH_BLOCKING");
-    if (local_rank_str) {
-      value = (std::stoi(local_rank_str) == 0);
-    }
-    return value;
-  }
-};
-#endif
-
 struct CommHelper {
   MPI_Comm comm;
 
